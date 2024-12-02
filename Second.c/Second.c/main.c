2d1ccea7b926e9d6d187ab985d6c0e1b4fb5d8f2
prayatna <shresthaprayatna1@gmail.com>
2024-12-02 20:48:43 +0545

    Added Tic Tac Toe Game

---
+++ Second.c/Second.c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Second.c
+//
+//  Created by Prayatna Shrestha on 23/11/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
